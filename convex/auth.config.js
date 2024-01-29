d3a5d4417c24797e47e5f469795fb2dc995c93a9
markande98 <gauravbpit1998@gmail.com>
2024-01-29 19:17:13 +0530

    feat: convex setup and clerk

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  providers: [
+    {
+      domain: "https://relaxing-amoeba-87.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
