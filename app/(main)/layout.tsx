e3cb5346d1b24adef759d69c4fe1ab19ba10dcea
markande98 <gauravbpit1998@gmail.com>
2024-01-30 19:16:00 +0530

    feat: sidebar move

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
+
+const MainLaypuut = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+  return (
+    <div className="h-full flex dark:[#1F1F1F]">
+      <Navigation />
+      <main className="h-full flex-1 overflow-y-auto">{children}</main>
+    </div>
+  );
+};
+
+export default MainLaypuut;
