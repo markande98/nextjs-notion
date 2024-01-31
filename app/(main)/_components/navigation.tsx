33fc3c953e39a60cf13eda682f61dcfbb1010a72
markande98 <gauravbpit1998@gmail.com>
2024-01-31 21:15:35 +0530

    feat/6: recursive document list in action

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,13 +12,13 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { useMutation } from "convex/react";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -145,9 +145,7 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
