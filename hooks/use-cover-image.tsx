3d5e93a5da7d558dbb2ae94129f4c95b56720a83
markande98 <gauravbpit1998@gmail.com>
2024-02-04 14:52:27 +0530

    feat/15: replace and delete cover image

--- hooks/use-cover-image.tsx
+++ hooks/use-cover-image.tsx
@@ -1,13 +1,17 @@
 import { create } from "zustand";
 
 type coverImgageStore = {
+  url?: string;
   isOpen: boolean;
   onClose: () => void;
   onOpen: () => void;
+  onReplace: (url: string) => void;
 };
 
 export const useCoverImage = create<coverImgageStore>((set) => ({
+  url: undefined,
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
+  onClose: () => set({ isOpen: false, url: undefined }),
+  onReplace: (url: string) => set({ isOpen: true, url }),
 }));
